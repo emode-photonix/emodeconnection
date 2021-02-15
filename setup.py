0863bc2caeaee11a85e1c9cbfc90cadb29b625a9
Eric Stanton <EModeSolver@gmail.com>
2021-02-15 07:15:26 -0700

    b2

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="emodeconnection",
-    version="1.0.0b1",
+    version="1.0.0b2",
     author="EMode Photonix LLC",
     author_email="EModeSolver@gmail.com",
     description="Python connection for EMode",
