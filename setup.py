965c3d00349c0a1cf4fc9ec3dc2cbf1237c00a3a
emode-photonix <emodesolver@gmail.com>
2025-03-16 08:25:07 -0600

    updating setup.py

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="emodeconnection",
-    version="1.0.7",
+    version="1.0.8",
     author="EMode Photonix LLC",
     author_email="hello@emodephotonix.com",
     description="Python connection for EMode",
